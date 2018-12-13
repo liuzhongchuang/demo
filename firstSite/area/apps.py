17ae84a35fff4359546ff27611ccadfbd0ec70ac
liuzhongchuang <3170449812@qq.com>
2018-12-13 16:47:49 +0800

    hadoop操作文档

--- firstSite/area/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AreaConfig(AppConfig):
-    name = 'area'
--- firstSite/login/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-from blog.models import Department
-
-
-
-Department().operate_Dept.creat_dept()
-Department().operate_Dept.getDeptById()
